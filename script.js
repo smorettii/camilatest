d9453f4c8618a30f59b3a71e5d322c47be65ebcd
moretti <132162213+smorettii@users.noreply.github.com>
2024-05-17 13:36:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const texto = document.querySelector("#texto")
 const button = document.querySelector("#transcrever")
 
-button.addEventListener("click", () => {
+button.addEventListener("click", async () => {
     const linhas = (texto.value).split("\n")
     let final = ''
 
@@ -13,10 +13,21 @@ button.addEventListener("click", () => {
             if (!(Number(sp[0]) && Number(sp[1]))) {
                 final = final + v + '\n'
             } else {
-                final = final
+                final = final.substring(0, final.length-1)
+                final = final + ' '
             }
         }
     }
+
     
+    
+    texto.value = final
+
+    await new Promise(result => setTimeout(result, 100))
+
+    for (var i=0; i<=100; i++) {
+        final = final.replace('\n*trocar*', ' ')
+    }
+
     texto.value = final
 })
